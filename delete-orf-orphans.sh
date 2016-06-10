aae61f2c35f64bb0a452a6e194818f331b7aac41
O Libre <olibre@Lmap.org>
2016-06-10 03:00:54 +0200

    Reduce indentation (and cleaning)

--- delete-orf-orphans.sh
+++ delete-orf-orphans.sh
@@ -19,13 +19,13 @@ jpg_names=$( sed 's|.*/\([^.]*\)[.][^.]\+$|\1|' <<< "$jpg_files" )
 orf_uniqs=$( sort -u <<< "$orf_names" )
 jpg_uniqs=$( sort -u <<< "$jpg_names" )
 
-orphans=$(      while read orf
-                do
-                        if ! fgrep -sq "$orf" <<< "$jpg_uniqs"
-                        then
-                                echo "$orf"
-                        fi
-                done <<< "$orf_uniqs"
+orphans=$(  while read orf
+            do
+                if ! fgrep -sq "$orf" <<< "$jpg_uniqs"
+                then
+                    echo "$orf"
+                fi
+            done <<< "$orf_uniqs"
         )
 
 [[ -z $orf_files ]] && count_orf=0 || count_orf=$( wc -l <<< "$orf_files" )
@@ -35,63 +35,76 @@ orphans=$(      while read orf
 echo -e "
 Found:
 \t$count_orf files '*.ORF'
-\t$count_jpg files '*.JPG' '*.JPEG' (case insesitive)  
+\t$count_jpg files '*.JPG' '*.JPEG' (case insesitive)
 \t$count orphans '*.ORF'
 "
 
 if [[ $count -eq 0 ]]
 then
-        echo >&2 'No ORF orphans found => exit'
-        exit
+    echo >&2 'No ORF orphans found => exit'
+    exit
 fi
 
-echo "Checking filenames..."
+
+
+echo 'Checking filenames...'
+
+find_pattern()
+{
+    id="$1"
+    if [[ $dir_orf == $dir_jpg ]]
+    then
+        find "$dir_orf" -type f -name "*$id*"
+    else
+        find "$dir_orf" "$dir_jpg" -type f -name "*$id*"
+    fi | wc -l
+}
 
 # Basic check
 error=0
 while read orph
 do
-        count=$( find "$dir_orf" "$dir_jpg" -type f -name "*$orph*" | wc -l )
-        if [[ $count -ne 1 ]]
-        then
-                echo >&2 "ERROR: Name '$orph' has been determined as unique ORF files. Therefore there should be one single file having '$orph' in its name. But count is '$count' (should be 1)"
-                set +e
-                let error++
-                set -e
-        fi
+    count=$( find_pattern "$orph" )
+    if [[ $count -ne 1 ]]
+    then
+        echo >&2 "ERROR: Pattern '$orph' has been determined as identifing an orphan ORF files. But there are '$count' filenames containing the pattern (should be 1)"
+        set +e
+        let error++
+        set -e
+    fi
 done <<< "$orphans"
 
 if (( $error ))
 then
-        echo >&2 "Found '$error' error(s) => exit"
-        exit
+    echo >&2 "Found '$error' error(s) => exit"
+    exit
 fi
 
 orphan_files=
 while read orph
 do
-        filename=$( fgrep "/$orph".ORF <<< "$orf_files" )
-        if [[ -f "$filename" ]]
-        then
-                if [[ -z $orphan_files ]]
-                then    # First insertion
-                        orphan_files="$filename"
-                else    # Append
-                        orphan_files="$orphan_files
+    filename=$( fgrep "/$orph".ORF <<< "$orf_files" )
+    if [[ -f "$filename" ]]
+    then
+        if [[ -z $orphan_files ]]
+        then    # First insertion
+            orphan_files="$filename"
+        else    # Append
+            orphan_files="$orphan_files
 $filename"
-                fi
-        else
-                echo >&2 "ERROR: Name '$orph' has been determined as unique ORF files. But cannot find file in directory '$dir_orf'."
-                set +e
-                let error++
-                set -e
         fi
+    else
+        echo >&2 "ERROR: Name '$orph' has been determined as unique ORF files. But cannot find file in directory '$dir_orf'."
+        set +e
+        let error++
+        set -e
+    fi
 done <<< "$orphans"
 
 if (( $error ))
 then
-        echo >&2 "Found '$error' error(s) => exit"
-        exit
+    echo >&2 "Found '$error' error(s) => exit"
+    exit
 fi
 
 # TODO check TTY [[ -t 0 ]] [[ -t 1 ]]
@@ -100,15 +113,15 @@ read -n 1 -p "
 Ready to delete '$count' ORF orphans? (y/n)" answer
 if [[ $answer != [yY] ]]
 then
-        echo >&2 "Answer '$answer' is not [yY] => exit"
-        exit
+    echo >&2 "Answer '$answer' is not [yY] => exit"
+    exit
 fi
 
 while read orph
 do
-        if ! rm -v "$orph" 
-        then
-                echo >&2 "ERROR while removing '$orph' => exit"
-                exit
-        fi        
+    if ! rm -v "$orph"
+    then
+        echo >&2 "ERROR while removing '$orph' => exit"
+        exit
+    fi
 done <<< "$orphan_files"
